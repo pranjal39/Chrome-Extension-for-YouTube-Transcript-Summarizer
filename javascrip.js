19ab2da11b326d047a2664032e4e02d418af1bea
pranjal_AWS <88576113+pranjal39@users.noreply.github.com>
2022-05-31 15:44:21 +0530

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,8 @@
+/**$.getJSON("dat.json", function(json) {
+    var a=json.subsContent; // this will show the info it in firebug console
+	var b=json.subsTime;
+	var c=json.summarySentences;
+	var d=json.summaryTime;
+	document.getElementById("test").innerHTML =a[0];
+});
+*/
